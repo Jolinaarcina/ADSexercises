c5094a1fa7b7403e3908e66d779cb26cca36de0f
Jolina Arcina <jolinaarcina2@gmail.com>
2021-02-19 00:26:24 +0800

    added controller

--- app/Http/Controllers/MyfirstController.php
+++ app/Http/Controllers/MyfirstController.php
@@ -6,12 +6,5 @@ use Illuminate\Http\Request;
 
 class MyfirstController extends Controller
 {
-    public function index()
-    {
-    	 $sum= 5+2;
-    	 return view('resultpage', [
-    	 	'result'=> $sum,
-    	 	'title'=> "Sample page title"
-    	 ]);
-    }
+    //
 }
