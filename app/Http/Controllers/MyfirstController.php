e71c53783bce40fb3d31ebb1adce60a86ec3f2e2
Jolina Arcina <jolinaarcina2@gmail.com>
2021-02-19 00:37:53 +0800

    added views

--- app/Http/Controllers/MyfirstController.php
+++ app/Http/Controllers/MyfirstController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class MyfirstController extends Controller
 {
-    //
+     public function index()
+    {
+    	 $sum= 5+2;
+    	 return view('resultpage', [
+    	 	'result'=> $sum,
+    	 	'title'=> "Sample page title"
+    	 ]);
+    }
 }
+
+
